ff1b6cce9a9a12cf3275186cec9318212f03de5b
Sandra <grahlsan@gmail.com>
2019-03-22 12:47:59 +0000

    added fullscreen canvas to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,6 @@ import Canvas from "../components/canvas"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Canvas />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
