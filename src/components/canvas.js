035702a6c18cddc21123defc6fc808187c1d2318
Sandra <grahlsan@gmail.com>
2019-03-27 21:02:55 +0000

    100 mini circles in randomised positions

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,58 +1,80 @@
 import React from "react"
 
-const circ = (props) => {
-    const {ctx, x, y, radius, startAngle, endAngle, drawCounterClockwise} = props;
+function Circle(x, y, dx, dy, radius, color) {
 
-    ctx.beginPath();
-    ctx.arc(x, y, radius, startAngle, endAngle, drawCounterClockwise);
-    ctx.strokeStyle = "#"+((1<<24)*Math.random()|0).toString(16);
-    return ctx.stroke();
+const draw = ctx => {
+    ctx.beginPath()
+    ctx.arc(x, y, radius, Math.PI * 2, false)
+    ctx.strokeStyle = color;
+    ctx.stroke()
+  }
+
+    this.update = (ctx) => {
+    if (x + radius > window.innerWidth || x - radius < 0) {
+      dx = -dx
+    }
+    if (y + radius > window.innerHeight || y - radius < 0) {
+        dy = -dy
+    }
+    x += dx
+    y += dy
+
+    draw(ctx);
+  }
 }
 
 class Canvas extends React.Component {
-    componentDidMount() {
-        this.updateCanvas();
-    }
-    componentDidUpdate() {
-        this.updateCanvas();
-    }
-    updateCanvas() {
-        const ctx = this.refs.canvas.getContext('2d');
+  constructor(props) {
+    super(props)
 
-
-    const animate = () => {
-
-        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
-        let radius = 10;
-        let z =  Math.random() * window.innerWidth;
-        let w = Math.random() * window.innerHeight;
-        let dx = (Math.random() - 0.5);
-        let dy = (Math.random() - 0.5);
-
-        if (z + radius > window.innerWidth || z - radius < 0) {
-           dx = -dx;
-        }
-            if (w + radius > window.innerHeight || w - radius < 0) {
-                dy = -dy;
-            }
-        circ({ctx, x: z += dx, y: w += dy , radius: radius, startAngle: 0, endAngle: Math.PI * 2, drawCounterClockwise: false});
-
-        }
-
-        setInterval(() => { requestAnimationFrame(animate); }, 3000);
-        
-
-    }
-
-    render() {
-      return (
-        <canvas
-          width={window.innerWidth}
-          height={window.innerHeight}
-          ref="canvas"
-            />
-      );
+    this.state = {
+      circleArray: Array.from({ length: 100 }).map(
+        () =>
+          new Circle(
+            Math.random() * (window.innerWidth - 5 * 2) + 5,
+            Math.random() * (window.innerHeight - 5 * 2) + 5,
+            Math.random() - 0.5,
+            Math.random() - 0.5,
+            5,
+            "#" + (((1 << 24) * Math.random()) | 0).toString(16)
+          )
+      ),
     }
   }
-export default Canvas 
+
+  componentDidMount() {
+    const ctx = this.refs.canvas.getContext("2d")
+
+    this.updateCanvas()
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas()
+  }
+
+  updateCanvas() {
+    const animate = () => {
+      const ctx = this.refs.canvas.getContext("2d")
+      requestAnimationFrame(animate)
+
+      ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+
+      this.state.circleArray.forEach(element => {
+        element.update(ctx)
+      })
+    }
+
+    animate()
+  }
+
+  render() {
+    return (
+      <canvas
+        width={window.innerWidth}
+        height={window.innerHeight}
+        ref="canvas"
+      />
+    )
+  }
+}
+export default Canvas
