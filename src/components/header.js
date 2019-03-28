cdfc8266dc579ff10e98b93f6ce15e47a4877613
Sandra <grahlsan@gmail.com>
2019-03-28 15:14:27 +0000

    removed gatsby default starter header

--- src/components/header.js
+++ src/components/header.js
@@ -5,26 +5,16 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+      background: `transparent`,
+    }}>
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
         </Link>
       </h1>
     </div>
