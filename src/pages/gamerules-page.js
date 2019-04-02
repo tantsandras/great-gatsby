c95776c67567a549df35b131ab9f1124a8d38d23
Sandra <grahlsan@gmail.com>
2019-04-02 16:26:11 +0100

    added game rules page before game starts

---
+++ src/pages/gamerules-page.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import GameRules from "../components/gameRules"
+
+const GameRulesPage = () => (
+  <Layout>
+    <GameRules />
+  </Layout>
+)
+
+export default GameRulesPage
